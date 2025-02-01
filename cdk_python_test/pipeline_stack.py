b305684a8b8dfd5cde628c63dba1691b1bdd4215
juanga6 <juanga.fp@gmail.com>
2025-02-01 16:48:42 +0000

    Initial commit: CI/CD work

--- cdk_python_test/pipeline_stack.py
+++ cdk_python_test/pipeline_stack.py
@@ -1,6 +1,6 @@
 from aws_cdk import Stack
 from aws_cdk import (
-    aws_codecommit as codecommit,
+    pipelines as pipelines,
 )
 from constructs import Construct
 
@@ -9,9 +9,22 @@ class WorkshopPipelineStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Creates a CodeCommit repository called 'WorkshopRepo'
-        repo = codecommit.Repository(
-            self, "WorkshopRepo", repository_name="WorkshopRepo"
+        git_input = pipelines.CodePipelineSource.connection(
+            repo_string="juanga6/cdk-python-test",
+            branch="main",
+            connection_arn="arn:aws:codeconnections:eu-north-1:724772078988:connection/124f0516-1e00-4f7c-a97b-f3e2b5651142",
         )
 
-        # Pipeline code goes here
+        pipeline = pipelines.CodePipeline(
+            self,
+            "Pipeline",
+            synth=pipelines.ShellStep(
+                "Synth",
+                input=git_input,
+                commands=[
+                    "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
+                    "pip install -r requirements.txt",  # Instructs Codebuild to install required packages
+                    "cdk synth",
+                ],
+            ),
+        )
