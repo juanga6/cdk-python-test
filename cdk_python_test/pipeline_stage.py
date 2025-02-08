351f6e79e73c1e84c88974955d4fbabeb5fbfbd1
Juanga <juanga.fp@gmail.com>
2025-02-08 16:05:42 +0000

    Added minimal e2e testing

--- cdk_python_test/pipeline_stage.py
+++ cdk_python_test/pipeline_stage.py
@@ -5,7 +5,18 @@ from .cdk_python_test_stack import CdkPythonTestStack
 
 
 class WorkshopPipelineStage(Stage):
+    @property
+    def hc_endpoint(self):
+        return self._hc_endpoint
+
+    @property
+    def hc_viewer_url(self):
+        return self._hc_viewer_url
+
     def __init__(self, scope: Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        CdkPythonTestStack(self, "WebService")
+        service = CdkPythonTestStack(self, "WebService")
+
+        self._hc_endpoint = service.hc_endpoint
+        self._hc_viewer_url = service.hc_viewer_url
